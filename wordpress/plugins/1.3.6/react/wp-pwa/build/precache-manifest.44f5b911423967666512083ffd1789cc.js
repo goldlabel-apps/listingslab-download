4472f05d9c3e2385a68ef3c313f41744d2182c9c
listingslab <listingslab@gmail.com>
2021-10-09 10:13:57 +1000

    upgrades plugins

---
+++ wordpress/plugins/1.3.6/react/wp-pwa/build/precache-manifest.44f5b911423967666512083ffd1789cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "df39704b7db52285f33a",
+    "url": "/static/css/main.93e719f5.chunk.css"
+  },
+  {
+    "revision": "df39704b7db52285f33a",
+    "url": "/static/js/main.df39704b.chunk.js"
+  },
+  {
+    "revision": "0d70d710e1c74ff999ba",
+    "url": "/static/js/1.0d70d710.chunk.js"
+  },
+  {
+    "revision": "e25dd797578c92440c43",
+    "url": "/static/js/2.e25dd797.chunk.js"
+  },
+  {
+    "revision": "15962f24474a19914890",
+    "url": "/static/js/runtime~main.15962f24.js"
+  },
+  {
+    "revision": "9bc2cf03f25d1edd3c96acf70604725a",
+    "url": "/index.html"
+  }
+];
